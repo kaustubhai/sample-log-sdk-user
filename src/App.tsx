2cc35c6514d107a42b67e9e188715e1c21ef1639
kaustubhai <kaustubh229@gmail.com>
2025-03-10 17:32:48 +0530

    feat: fnf

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import chronolog from '@farmart-engineering/chronolog';
 
+// setting up the chronolog
+chronolog.setup({
+  production: true,
+  autoOpenSearch: true,
+  index: "farmartapp",
+  console: true,
+})
+
+// sample api call object
+const apiObj = {
+  apiMethod: "GET",
+  apiHeaders: {
+      "Content-Type": "application/json",
+  },
+  apiHost: "https://api.example.com",
+  apiResponseCode: 200,
+  apiResponseMessage: "Success",
+  meta: {
+      timeTaken: 23,
+  },
+  userId: "1234567890",
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => chronolog.log('From react')}>Normal Log</button>
+        <button onClick={() => chronolog.error(new Error('From react'))}>Error Log</button>
+        <button onClick={() => chronolog.event_track({
+          message: "This-is-an-event-message",
+          severityLevel: "EVE",
+        })}>Event log</button>
+        <button onClick={() => 
+          chronolog.log("API called successfully", apiObj)
+        }>API Log</button>
       </header>
     </div>
   );
