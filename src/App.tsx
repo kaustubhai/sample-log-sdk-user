b0d1916f5944430241a8e710303ddd7b71a988d2
kaustubhai <kaustubh229@gmail.com>
2025-03-25 16:08:04 +0530

    feat: updated logger implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,8 @@
 import './App.css';
-import { useEffect } from 'react';
+// import { useEffect } from 'react';
 import chronolog from '@farmart-engineering/chronolog';
-import { useLocation, useNavigationType } from 'react-router-dom';
-import { createRoutesFromChildren, matchRoutes } from 'react-router-dom';
-// setting up the chronolog
-chronolog.setup({
-  production: true,
-  autoOpenSearch: true,
-  index: "farmartapp",
-  console: true,
-  environment: "staging",
-  version: "0.2.0",
-  deviceType: "web",
-  origin: "Sample-Client",
-  sentry: {
-      dsn: '<DSN>',
-      environment: "development",
-      tracesSampleRate: 0.01,
-      replaysSessionSampleRate: 0.01,
-      replaysOnErrorSampleRate: 0.01,
-    reactRouter: {
-      useEffect: useEffect,
-      useLocation: useLocation,
-      useNavigationType: useNavigationType,
-      createRoutesFromChildren: createRoutesFromChildren,
-      matchRoutes: matchRoutes,
-    }
-  },
-  moengage: {
-    appId: "<APP-ID>",
-    appKey: "<APP-KEY>",
-  }
-})
+// import { useLocation, useNavigationType } from 'react-router-dom';
+// import { createRoutesFromChildren, matchRoutes } from 'react-router-dom';
 
 // sample api call object
 const apiObj = {
@@ -53,21 +24,27 @@ function handleClick() {
   throw new Error("Error from react boundary 3")
 }
 function App() {
+    // setting up the chronolog
+    console.log('first')
+    
+  console.log('second')
   return (
-    <div className="App">
-      <header className="App-header">
-        <button onClick={() => chronolog.log('From react')}>Normal Log</button>
-        <button onClick={handleClick}>Error Log</button>
-        <button onClick={() => chronolog.event_track({
-          message: "This-is-an-event-message",
-          userId: "1234567890",
-          appId: "CS2M99BHDFVE4TH87HIUXKP4"
-        })}>Event log</button>
-        <button onClick={() =>
-          chronolog.log("API called successfully", apiObj)
-        }>API Log</button>
-      </header>
-    </div>
+    // <chronolog.ErrorBoundary>
+      <div className="App">
+        <header className="App-header">
+          <button onClick={() => chronolog.log('From react')}>Normal Log</button>
+          <button onClick={handleClick}>Error Log</button>
+          <button onClick={() => chronolog.event_track({
+            message: "This-is-an-event-message",
+            userId: "7007992740",
+            appId: "K09VN3BYYO3G1HNLGR8G5F5M"
+          })}>Event log</button>
+          <button onClick={() =>
+            chronolog.log("API called successfully", apiObj)
+          }>API Log</button>
+        </header>
+      </div>
+    // </chronolog.ErrorBoundary>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,49 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import chronolog from '@farmart-engineering/chronolog';
+import { createRoutesFromChildren } from 'react-router-dom';
+import { useNavigationType } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import { matchRoutes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-console.log({chronolog: chronolog.ErrorBoundary})
+chronolog.initialize({
+  production: true,
+  autoOpenSearch: true,
+  index: "farmartapp",
+  console: true,
+  environment: "staging",
+  version: "0.2.0",
+  deviceType: "web",
+  origin: "Sample-Client",
+  sentry: {
+    dsn: '<DSN>>',
+    environment: "development",
+    tracesSampleRate: 0.01,
+    replaysSessionSampleRate: 0.01,
+    replaysOnErrorSampleRate: 0.01,
+    reactRouter: {
+      useEffect: useEffect,
+      useLocation: useLocation,
+      useNavigationType: useNavigationType,
+      createRoutesFromChildren: createRoutesFromChildren,
+      matchRoutes: matchRoutes,
+    }
+  },
+  moengage: {
+    appId: "<APP_ID>",
+    debugMode: 1,
+    moeDataCenter: "dc_3"
+  }
+})
 root.render(
   <React.StrictMode>
-    <chronolog.ErrorBoundary>
     <App />
-    </chronolog.ErrorBoundary>
   </React.StrictMode>
 );
 
